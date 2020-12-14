8d3e3e0cc61243e7aa024767b0e8e980b823e224
Lucas Santiago <lu.santi.oli@gmail.com>
2020-12-14 16:23:05 -0300

    feat(json): Correcting the json parse in the structs

--- structs/response.go
+++ structs/response.go
@@ -3,17 +3,25 @@ package structs
 // Response Struct to return the response of the Rede's API
 type Response struct {
 	// Return code
-	ReturnCode string `json:"ReturnCode"`
+	ReturnCode string `json:"returnCode"`
 	// Return message
-	ReturnMessage string `json:"ReturnMessage"`
+	ReturnMessage string `json:"returnMessage"`
 	// Reference to the Request.Reference code
-	Reference string `json:"Reference"`
+	Reference string `json:"reference"`
 	// Unique payment indentifier
-	Tid string `json:"Tid"`
+	Tid string `json:"tid"`
 	// Sequencial number returned by Rede
-	Nsu string `json:"Nsu"`
+	Nsu string `json:"nsu"`
 	// Authorization code by the card's manufactory
-	AuthorizationCode string `json:"AuthorizationCode"`
-	// Date Time
-
+	AuthorizationCode string `json:"authorizationCode"`
+	// DateTime time when rede processed the request
+	DateTime string `json:"dateTime"`
+	// Amount R$10,00 equals 1000
+	Amount int `json:"amount"`
+	// CardBin first 6 digits of the credit card
+	CardBin int `json:"cardBin"`
+	// Last4 last four digits of the credit card
+	Last4 int `json:"last4"`
+	// BrandTid
+	BrandTid string `json:"BrandTid"`
 }
